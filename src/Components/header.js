019515019fe57d01c54d726d9e3bb7916251e8fe
Suhaib Khan <suhaib0900@gmail.com>
2024-06-08 16:13:23 +0100

    Login Button ADded

--- src/Components/header.js
+++ src/Components/header.js
@@ -1,15 +1,15 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 import { LOGO_URL } from '../utils/constants';
-
+import { useState } from 'react';
 const Header = () => {
+  const [login, setLogin] = useState(false);
+  const hanldeLogin = () => {
+    setLogin(!login);
+  };
   return (
     <div className="header">
       <div className="logo-container">
-        <img
-          className="logo"
-          src={LOGO_URL}
-          alt="Food Delivery Logo"
-        />
+        <img className="logo" src={LOGO_URL} alt="Food Delivery Logo" />
       </div>
       <div className="nav-items">
         <ul>
@@ -17,6 +17,9 @@ const Header = () => {
           <li>About Us</li>
           <li>Contact</li>
           <li>Cart</li>
+          <li>
+            <button onClick={hanldeLogin}>{login ? `Logout` : `Login`}</button>
+          </li>
         </ul>
       </div>
     </div>
