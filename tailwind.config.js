af657e9cf0fd4bf31a9ca409164e2aee04604986
Suhaib Khan <suhaib0900@gmail.com>
2024-06-15 20:01:02 +0100

    Tailwind Config Change

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,7 @@
-
-// tailwind.config.js
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  purge: ['./src/**/*.{js,jsx,ts,tsx}', './index.html'],
-  darkMode: false, // or 'media' or 'class'
+  content: ['./src/**/*.{html,js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
-  },
-  variants: {
     extend: {},
   },
   plugins: [],
