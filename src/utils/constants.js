90c21af5cfab09cf7378d43f8d04e0776ac80f89
Suhaib Khan <suhaib0900@gmail.com>
2024-10-26 15:40:15 +0100

    Switched From Render To Azure

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -6,8 +6,12 @@ const LOGO_URL =
 //   'https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9352403&lng=77.624532&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_L';
 // const MENU_URL =
 //   'https://www.swiggy.com/mapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.9352403&lng=77.624532&restaurantId';
-const API_URL = 'https://foodapp-react-backend.onrender.com/restaurants';
-const MENU_URL = 'https://foodapp-react-backend.onrender.com/restaurants';
-const Payment_URL =
-  'https://foodapp-react-backend.onrender.com/create-checkout-session';
+//const API_URL = 'https://foodapp-react-backend.onrender.com/restaurants';
+//const MENU_URL = 'https://foodapp-react-backend.onrender.com/restaurants';
+//const Payment_URL ='https://foodapp-react-backend.onrender.com/create-checkout-session';
+
+
+const API_URL='https://foodapp-backend-fab6hggyd4enarhs.uksouth-01.azurewebsites.net/restaurants';
+const MENU_URL='https://foodapp-backend-fab6hggyd4enarhs.uksouth-01.azurewebsites.net/restaurants';
+const Payment_URL='https://foodapp-backend-fab6hggyd4enarhs.uksouth-01.azurewebsites.net/create-checkout-session';
 export { CDN_URL, LOGO_URL, API_URL, MENU_URL, Payment_URL };
